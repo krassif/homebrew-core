b3886ae4635cd443e690b92d218b8de158172737
Krassimir Fotev <krassimir.fotev@gmail.com>
2017-02-01 19:10:30 -0500

    Update haproxy.rb

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,31 +1,39 @@
-require 'formula'
-
 class Haproxy < Formula
-  homepage 'http://haproxy.1wt.eu'
-  url 'http://www.haproxy.org/download/1.5/src/haproxy-1.5.0.tar.gz'
+  desc "Reliable, high performance TCP/HTTP load balancer"
+  homepage "http://www.haproxy.org/"
+  url "http://www.haproxy.org/download/1.6/src/haproxy-1.6.0.tar.gz"
+  sha256 "e83a272b7d3638cf1d37bba58d3e75f497c1862315ee5bb7f5efc1d98d26e25b"
 
   bottle do
     cellar :any
-    sha256 "c9978b6fa1153a4b5d2873b2c0f738be53654ba30b133c4d3eb648f025fc54a3" => :sierra
-    sha256 "eb2e9f8b44983b54a993f6abae639bb3d3e12f69c7ae81298fc0870bc7feeb02" => :el_capitan
-    sha256 "ef89607a0342812850a845ccce1abcf055d08b1566d60aba04475a5098924b03" => :yosemite
+    sha256 "0f74dd1eab5f74db336b8b4853a849769588dd6777a2a56acd59d11cba5dbc8c" => :el_capitan
+    sha256 "54369189f1b23ee7240f82b1277293eae6fd583cbed0fc3c43f344477149f1a6" => :yosemite
+    sha256 "cc7b5f2b2a2eea08f7b78a15074fcccfa2c1a7e901ffda301ede6a2c479a1fbc" => :mavericks
+    sha256 "9695138dd5bd45cee081720f2ba0791c4f5fdfedf43d883770531c40eefa8377" => :mountain_lion
   end
 
-  depends_on 'pcre'
+  depends_on "openssl"
+  depends_on "pcre"
 
   def install
-    args = ["TARGET=generic",
-            "USE_KQUEUE=1",
-            "USE_POLL=1",
-            "USE_PCRE=1",
-            "USE_OPENSSL=1",
-            "USE_ZLIB=1",
-            "ADDLIB=-lcrypto",
+    args = %w[
+      TARGET=generic
+      USE_KQUEUE=1
+      USE_POLL=1
+      USE_PCRE=1
+      USE_OPENSSL=1
+      USE_ZLIB=1
+      USE_LUA=1
+      ADDLIB=-lcrypto
     ]
 
     # We build generic since the Makefile.osx doesn't appear to work
-    system "make", "CC=#{ENV.cc}", "CFLAGS=-I/usr/local/opt/openssl/include", "LDFLAGS=-L/usr/local/opt/openssl/lib",  *args
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "LDFLAGS=#{ENV.ldflags}", *args
     man1.install "doc/haproxy.1"
     bin.install "haproxy"
+  end
+
+  test do
+    system bin/"haproxy", "-v"
   end
 end
